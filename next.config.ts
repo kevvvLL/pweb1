c07241d6cb93c24cad1ce2b2f9336c2acf6cbc4c
kevvvLL <Lixbins@gmail.com>
2024-10-27 02:59:41 -0400

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  outputFileTracingExcludes: {
+    '*': [
+      'node_modules/@swc/core-linux-x64-gnu',
+      'node_modules/@swc/core-linux-x64-musl',
+      'node_modules/@esbuild/linux-x64',
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
